174bd6e7d4ff71e1dcd63055573cb42dc36b07be
Slygain <kelsoge@gmail.com>
2015-11-21 16:29:42 +1100

    Cleanup A
    removed alot of un-needed scripting code (appears that newer versions of
    ruby have gem integrated)

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,42 +7,28 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 
-#useful variables:
-appURL = 'git://github.com/tnh/simple-sinatra-app.git'
-
 #install required packages
 package 'ruby'
-package 'rdoc' #otherwise gem install fails
 package 'git'
-
-
-#install gem
-# grab latest copy off github
-git '/tmp/rubyGem' do
-  repository 'git://github.com/rubygems/rubygems.git'
-  revision 'master'
-  action :checkout
-end
-
-# run the gem installer
-execute 'gemInstall' do
-  command 'ruby /tmp/rubyGem/setup.rb'
-  ignore_failure true
-end
 
 #install sinatra
 gem_package 'sinatra' do
   action :install
 end
 
-#get current copy of sinatra application
-git '/tmp/rubyGem' do
-  repository 'git://github.com/rubygems/rubygems.git'
+#install bundler
+gem_package 'bundler' do
+  action :install
+end
+
+#get copy of sinatra application
+git '/tmp/sinatraApp' do
+  repository 'git://github.com/tnh/simple-sinatra-app.git'
   revision 'master'
   action :checkout
 end
 
-#deploy copy of sinatra application
+#place Sinatra app in decent location
 
 #clean up any install files from sinatra application
 
