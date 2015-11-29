acef5f940f59629cd76ae28d7339610b72ddf987
Slygain <kelsoge@gmail.com>
2015-11-29 12:55:46 +1100

    rewrote to use httpd instead
    re-wrote entire deploy to use http instead. simpler and less headaches
    from research gathered during the week

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,136 +9,77 @@
 # improvements that can be made:
 # setup variables with known file locations so recipe can re-used for other applications
 # Currently assumes user is root
-
-#add passenger repository
-execute 'Phusion addition' do
-  command 'curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo'
-end
-
-# work around for having a new repository and needing it to accept the gpg key
-execute 'manual Ruby Install' do
-  command 'yum install ruby -y'
-end
-
-#install required packages
+package 'ruby'
 package 'git'
-# needed to allow us to install nginx & Phusion
-package 'epel-release'
-package 'nginx'
-package 'passenger'
+package 'httpd'
+
+#pasenger requirements
+package 'gcc'
+package 'gcc-c++'
+package 'zlib-devel'
+package 'libcurl-devel'
+package 'httpd-devel'
+package 'ruby-devel'
+package 'apr-devel'
+package 'apr-util-devel'
 
 
-# work out how to manipulate /etc/nginx/conf.d/passenger.conf - uncomment 3 lines
-# TODO: May not need to do this afterall
-
-#setup services related to nginx
-service 'nginx' do 
+service 'httpd' do
   action [ :enable, :start ]
 end
 
-#install sinatra
-gem_package 'sinatra' do
-  action :install
+gem_package 'passenger'
+gem_package 'bundler'
+
+execute 'configure passenger' do
+  command 'passenger-install-apache2-module -a'
 end
 
-#install bundler
-gem_package 'bundler' do
-  action :install
+#create configuration for apache (httpd)
+file '/etc/httpd/conf.d/sinatra.conf' do
+ content ' LoadModule passenger_module /usr/local/share/gems/gems/passenger-5.0.21/buildout/apache2/mod_passenger.so
+   <IfModule mod_passenger.c>
+     PassengerRoot /usr/local/share/gems/gems/passenger-5.0.21
+     PassengerDefaultRuby /usr/bin/ruby
+   </IfModule>
+   
+  <VirtualHost *:80>
+    ServerName localhost
+    DocumentRoot /var/www/apps/sinatraApp/public
+    <Directory /var/www/apps/sinatraApp/public>
+        Require all granted
+        Allow from all
+        Options -MultiViews
+    </Directory>
+</VirtualHost> 
+'
 end
 
-#get copy of sinatra application
-git '/tmp/sinatraApp' do
+execute 'create sinatraApp Directorys' do
+    command "mkdir -p /var/www/apps"
+    user "root"
+end
+
+git '/var/www/apps/sinatraApp' do
   repository 'git://github.com/tnh/simple-sinatra-app.git'
   revision 'master'
   action :checkout
 end
-# Move out application somewhere more sane
-execute "create sinatraApp Directory" do
-    command "mkdir -p ~/apps"
-    user "root"
-end
-execute "copy sinatraApp" do
-    command "cp -rf /tmp/sinatraApp ~/apps/sinatraApp"
-    user "root"
-end
- 
-#install bundle from sinatra application
+
 execute 'bundle install' do
-  cwd '/root/apps/sinatraApp'
+  cwd '/var/www/apps/sinatraApp'
   command 'bundle install'
 end
 
-# write over the top of the nginx configuration file
-#TODO: shift to template.
-# grabs the passenger broadcast from port 3000 and pushes it across to port 80
-file '/etc/nginx/nginx.conf' do
-  content '
-worker_processes  1;
-events {
-    worker_connections  1024;
-}
-
-http {
-    include       mime.types;
-    default_type  application/octet-stream;
-    sendfile        on;
-    keepalive_timeout  65;
-    
-    server {
-        listen 80;
-        server_name localhost;
-        
-        location ~ \ {
-        proxy_pass http://127.0.0.1:3000;
-        }
-
-        # redirect server error pages to the static page /50x.html
-        error_page   500 502 503 504  /50x.html;
-        location = /50x.html {
-            root   html;
-        }
-    }
-}'
+execute 'create App directories' do
+  command 'mkdir /var/www/apps/sinatraApp/public; mkdir /var/www/apps/sinatraApp/tmp'
 end
 
-#reload nginx 
-execute "reload Nginx" do
-    command "service nginx restart"
-    user "root"
-end
-
-#create a service for our passenger
-file '/usr/lib/systemd/system/sinatraApp.service' do
-    content'
-    [Unit]
-Description=The sinatraApplication
-Wants=network-online.target
-After=network-online.target
-
-[Service]
-Type=simple
-ExecStart=/usr/bin/passenger start -d ~/apps/sinatraApp
-ExecReload=/usr/bin/passenger stop; /usr/bin/passenger start -d ~/apps/sinatraApp
-KillMode=process
-# Sleep for 1 second to give PassengerAgent a chance to clean up.
-ExecStop=passenger stop; /bin/sleep 1
-PrivateTmp=true
-
-[Install]
-WantedBy=multi-user.target
-
-    '
-end
-
-# setup passenger service
-service "sinatraApp" do
-    action [ :enable, :start ]
-end
-
-# allows port 80 to be accessible externally
 execute "add firewall rules" do
 command 'firewall-cmd --permanent --zone=public --add-service=http ;firewall-cmd --permanent --zone=public --add-service=https;firewall-cmd --reload '
 end
 
-
+service 'httpd' do
+  action [:restart]
+end
 
